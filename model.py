d7d6411148389cba4a474c51cc17ab766d6b99cd
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-02 12:37:55 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,16 @@
-import os
 import google.generativeai as genai
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+# api_key = 'AIzaSyAHHXYaCKoa67OQc5_6eOtGnkeuZUcudjU'
+
+# genai(api_key=api_key)
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-pro")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def ask_gemini(context, question):
     prompt = f"""
