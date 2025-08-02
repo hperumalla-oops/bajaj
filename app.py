f443e19b09222986638ab40af4f122a8c691c8d9
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-02 15:29:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from retriever import get_relevant_chunks
 from model import ask_gemini
@@ -16,9 +15,13 @@ async def run_rag(request: RAGRequest):
     responses = []
     for q in request.questions:
         context = chunks.get(q, "")
-        answer = ask_gemini(context, q)
-        responses.append({"question": q, "answer": answer})
-    return {"responses": responses}
+        lol = ask_gemini(context, q)
+        answer = lol.text.strip() if lol.text else "No answer found."
+        responses.append(answer)
+        
+    return {"answers": responses}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000)
+
+
