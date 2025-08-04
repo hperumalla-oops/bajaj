d4d5f66a5d77fc5ebe866aeaa053babc596d4879
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-08-04 23:22:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,27 @@
+import pdfplumber
+import requests
+import tempfile
+
+
 def chunk_text(text, chunk_size=300):
     words = text.split()
     return [
         " ".join(words[i:i+chunk_size])
         for i in range(0, len(words), chunk_size)
     ]
+
+
+def download_pdf_from_url(url):
+    response = requests.get(url)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+        tmp.write(response.content)
+        return tmp.name
+    
+
+def extract_text_from_pdf(path):
+    text = ""
+    with pdfplumber.open(path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() + "\n"
+    return text
+
